34172020e38c81f6dbc76c3257195c1d50635237
osamarashad_1988 <soma76968@gmail.com>
2019-08-09 11:43:32 +0200

    update
    fixing

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-  
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-}
 makeCacheMatrix <- function( m = matrix() ) {
   i <- NULL
   set <- function( matrix ) {
